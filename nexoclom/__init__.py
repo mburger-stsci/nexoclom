832a1894726a678dabd53a448255abcb0f16479d
Matthew Burger <mburger@stsci.edu>
2019-06-04 09:51:32 -0400

    Something

--- nexoclom/__init__.py
+++ nexoclom/__init__.py
@@ -10,7 +10,7 @@ from .IDLout import IDLout
 name = 'nexoclom'
 __author__ = 'Matthew Burger'
 __email__ = 'mburger@stsci.edu'
-__version__ = '1.1.5'
+__version__ = '1.1.6'
 database = 'thesolarsystemmb'
 
 
