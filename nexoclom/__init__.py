c4ba7af76e7dcd9e051da2bd3dfd3b3d6ba5a7f4
Matthew Burger <mburger@stsci.edu>
2021-09-28 13:56:05 -0400

    Merged changed import statements branch

--- nexoclom/__init__.py
+++ nexoclom/__init__.py
@@ -9,7 +9,7 @@ from nexoclom.solarsystem import SSObject
 __name__ = 'nexoclom'
 __author__ = 'Matthew Burger'
 __email__ = 'mburger@stsci.edu'
-__version__ = '3.0.6'
+__version__ = '3.0.7'
 __date__ = '2021-09-27'
 
 configure()
