8bee1d66a4d1d405ad0f86aa619997bca0af1122
Matthew Burger <mburger@stsci.edu>
2022-06-02 14:24:01 -0400

    path changes in ModelResult

--- nexoclom/__init__.py
+++ nexoclom/__init__.py
@@ -9,7 +9,7 @@ from nexoclom.solarsystem import SSObject
 __name__ = 'nexoclom'
 __author__ = 'Matthew Burger'
 __email__ = 'mburger@stsci.edu'
-__version__ = '3.2.8'
-__date__ = '2022-05-27'
+__version__ = '3.2.9'
+__date__ = '2022-06-02'
 
-configure(verbose=True)
+configure(verbose=False)
