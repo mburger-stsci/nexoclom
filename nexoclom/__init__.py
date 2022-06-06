6ac848d5f1f42b50b79bd94d3df1e513eaeb51d8
Matthew Burger <mburger@stsci.edu>
2022-06-06 15:01:22 -0400

    updated version #

--- nexoclom/__init__.py
+++ nexoclom/__init__.py
@@ -9,7 +9,7 @@ from nexoclom.solarsystem import SSObject
 __name__ = 'nexoclom'
 __author__ = 'Matthew Burger'
 __email__ = 'mburger@stsci.edu'
-__version__ = '3.2.13'
-__date__ = '2022-06-03'
+__version__ = '3.2.14'
+__date__ = '2022-06-06'
 
 configure(verbose=False)
